b7eb26463038d08c168922df7aad27926526f762
Happy-Ferret <warfan2007@gmail.com>
2022-09-03 19:41:41 +0200

    Integrate Consent Manager
    - Extend Docusaurus theme
    - Implement basic profile integration
    - Implement YouTube integration
      - Create new component instead of
        using packaged YouTube integration,
        for translation purposes

---
+++ src/components/ConsentManager/integration-youtube.js
@@ -0,0 +1,27 @@
+import {
+    createIconComponentFromSimpleIconsSvgPath,
+    getForegroundColor
+  } from '@consent-manager/core'
+  
+  import YouTube from 'simple-icons/icons/youtube'
+  
+  export function youtubeIntegration() {
+    const { title, slug, hex, path } = YouTube
+    const color = `#${hex}`
+    const contrastColor = getForegroundColor(color)
+    const lang =
+      typeof window !== 'undefined' ? window.navigator.language : 'en-US'
+    const Icon = createIconComponentFromSimpleIconsSvgPath(title, path)
+  
+    return {
+      id: slug,
+      title,
+      category: 'Videos',
+      color,
+      contrastColor,
+      Icon,
+      privacyPolicyUrl: `https://policies.google.com/privacy?hl=${lang}`,
+      description:
+        'Enjoy the videos and music you love, upload original content, and share it all with friends, family, and the world on YouTube.',
+    }
+  }
