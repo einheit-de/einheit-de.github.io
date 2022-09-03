1acb64f408ff102d7ef47a2e730ec887fc323524
Happy-Ferret <warfan2007@gmail.com>
2022-09-03 20:35:27 +0200

    Update audio player
    - Add `showCover` prop (default: false)
    - Save theme state

--- src/components/AudioPlayer/index.js
+++ src/components/AudioPlayer/index.js
@@ -7,6 +7,8 @@ import BrowserOnly from "@docusaurus/BrowserOnly";
 
 // Use <Translate> for Zuhören/Play and for react-music-player specific locale translations.
 // Global Event Listener: "ThemeChange"
+// TODO: Restore volume (className: .play-sounds) for max-width: 767px
+// TODO: Replace document.getElementsByClassName with React equivalent
 export default class AudioPlayer extends React.Component {
   constructor(props) {
     super(props);
@@ -16,7 +18,7 @@ export default class AudioPlayer extends React.Component {
       options: {
         audioLists: this.props.playlist,
         defaultPlayIndex: 0,
-        theme: "auto",
+        theme: this.theme,
         autoPlay: false,
         defaultPosition: { top: 80, right: 80 },
         glassBg: true,
@@ -30,21 +32,39 @@ export default class AudioPlayer extends React.Component {
     };
   }
 
-  play = (e) => {
+  get theme() {
+    if (typeof window != "undefined") {
+      return localStorage.getItem("audioTheme")
+    }
+  }
+
+  set theme(themeName) {
+    if (typeof window != "undefined") {
+      localStorage.setItem('audioTheme', themeName)
+    }
+  }
+
+  play = () => {
     this.setState({ open: this.state.open ? false : true })
     return this.state.open
       ? this.state.instance.pause()
       : this.state.instance.play()
   }
 
+  changeTheme = (theme) => {
+    this.theme = theme
+  }
+
   componentDidUpdate() {
     let playlist = document.getElementsByClassName("group audio-lists-btn")[0]
     let prev = document.getElementsByClassName("group prev-audio")[0]
     let next = document.getElementsByClassName("group next-audio")[0]
+    let cover = document.getElementsByClassName("img-content img-rotate img-rotate-pause")[0]
 
     this.props.showPlaylist ? playlist.style.display = "" : playlist.style.display = "none"
     this.props.showPrev ? prev.style.display = "" : prev.style.display = "none"
     this.props.showNext ? next.style.display = "" : next.style.display = "none"
+    this.props.showCover ? cover.style.display = "" : cover.style.display = "none"
   }
 
   render() {
@@ -57,7 +77,9 @@ export default class AudioPlayer extends React.Component {
         : <Translate id="buttonListen">🎧 Zuhören</Translate>} </button>
       <BrowserOnly>
         {() => {
+          if (this.theme == null) { this.theme = "auto" }
           return <ReactJkMusicPlayer
+            onThemeChange={this.changeTheme}
             style={{ display: this.state.open ? "block" : "none" }}
             getAudioInstance={(instance) => { this.state.instance = instance }}
             mobileMediaQuery="(max-width: 0px)"
